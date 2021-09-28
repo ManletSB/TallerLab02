d2cc3ef86fb06e364e82cfb2c84511dd29892bbc
ninodl <n.diaz10@ufromail.cl>
2021-09-28 09:10:27 -0300

    Crear metodo menu crear metodo mayorProductoAdyacentes

---
+++ Arreglo/src/main/java/Arreglo02.java
@@ -0,0 +1,18 @@
+public class Arreglo02 {
+    public static void main(String[] args) {
+
+        
+    }
+
+    public static void mostrarMenu() {
+        System.out.println("Bienvenido al menú :D ");
+        System.out.println("----------------------------------------");
+        System.out.println("¿Qué desea hacer? ");
+        System.out.println("[1] Mostrar pares adyacentes ");
+        System.out.println("[2] Mostrar el mayor valor de producto ");
+        System.out.println("[3] Salir ");
+        System.out.println("----------------------------------------");
+    }
+
+
+}
