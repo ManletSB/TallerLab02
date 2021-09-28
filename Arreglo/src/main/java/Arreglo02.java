5752cd4c0fd9f2699455e23117a3daeba5cb40a9
ninodl <n.diaz10@ufromail.cl>
2021-09-28 10:32:31 -0300

    Añadido metodos de clase manlet

--- Arreglo/src/main/java/Arreglo02.java
+++ Arreglo/src/main/java/Arreglo02.java
@@ -1,7 +1,57 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Arreglo02 {
     public static void main(String[] args) {
 
 
+    }
+    public static int ingresarLargoArreglo(){
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresar el largo de su arreglo");
+
+        return teclado.nextInt();
+    }
+
+    public static int[] generarArreglo(int largo){
+
+        if(largo < 2 || largo > 10){
+            return null;
+        }
+
+        return new int[largo];
+    }
+
+    public static int[] llenarArreglo(int[] myArr){
+
+        for(int i = 0; i < myArr.length; i++){
+            myArr[i] = new Random().nextInt(100);
+        }
+
+        return myArr;
+    }
+
+    public static void mostrarParesAdyacentes(int[] myArr) {
+        System.out.println("Los pares adyacentes son: ");
+        for (int i = 1; i < myArr.length; i++) {
+            System.out.println("(" + myArr[i-1] +","+myArr[i]+")");
+        }
+    }
+    public static String darEntrada() {
+
+        System.out.println("Ingrese un número");
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static Integer darEntradaInt() {
+
+        try {
+            return Integer.parseInt(darEntrada());
+        } catch (NumberFormatException e) {
+            System.out.println("ERROR: vuelva a ingresar el número");
+            return darEntradaInt();
+        }
     }
 
     public static void mostrarMenu() {
@@ -13,7 +63,35 @@ public class Arreglo02 {
         System.out.println("[3] Salir ");
         System.out.println("----------------------------------------");
     }
+    public static void iniciar() {
 
+        int eleccion = -1;
+
+        do {
+            int[] arreglo = llenarArreglo(generarArreglo(ingresarLargoArreglo()));
+            mostrarMenu();
+            eleccion = darEntradaInt();
+
+            switch (eleccion) {
+                case 1 -> {
+                    mostrarParesAdyacentes(arreglo);
+                }
+                case 2 -> {
+                    System.out.println(mayorProductoAdyacentes(arreglo));
+                }
+                case 3 -> {
+                    salir();
+                }
+                default -> System.out.println("Se ha ingresado una opción no válida");
+            }
+
+        } while (eleccion != 3);
+    }
+    public static void salir() {
+        System.out.println("Gracias por usar la aplicacion");
+        System.out.println("Adios ...");
+
+    }
 
 
     public static int mayorProductoAdyacentes(int[] myArr) {
