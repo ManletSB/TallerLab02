d10531fb230c2e2260e5958d0baf7ed1c8a826a2
Manlet Sánchez <manlet.sanchez@gmail.com>
2021-09-28 09:08:31 -0300

    Nos dividimos el trabajo, yo haré hasta ahora: - ingresarLargoArreglo - generarArreglo - mostrarParesAdyacentes

---
+++ Arreglo/src/main/java/Arreglo01.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Arreglo01 {
+
+    public static int ingresarLargoArreglo(){
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingresar el largo de su arreglo");
+
+        return teclado.nextInt();
+    }
+
+    public static int [] generarArreglo(){
+
+
+
+    }
+}
